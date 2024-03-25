6b6bfb8d15bfb66ff80fa3d9529a186f2dd338be
ben <ben.random0@proton.me>
2024-03-25 21:34:43 +0100

    added comman line interface for specified number of tabs to open

--- searchpypi.py
+++ searchpypi.py
@@ -1,22 +1,36 @@
 #! python3
 #! searchpypi.py - Opens several search results.
 
-# Usage: python3 searchpypi.py <search term>
+import argparse
+import requests
+import sys
+import webbrowser
+import bs4
 
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Opens several search results from PyPI.')
+    parser.add_argument('search_term', metavar='search_term', type=str, nargs='+', help='The term to search on PyPI')
+    parser.add_argument('-n', '--tabs', type=int, default=5, help='Number of browser tabs to open (default: 5)')
+    args = parser.parse_args()
 
-import requests, sys, webbrowser, bs4
+    search_term = ' '.join(args.search_term)
+    num_tabs = args.tabs
 
-print('Searching...') # Show user what is going on while searching the web
+    print('Searching...') # Show user what is going on while searching the web
 
-res = requests.get('https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
-res.raise_for_status()
+    res = requests.get('https://pypi.org/search/?q=' + search_term)
+    res.raise_for_status()
 
-# Retrieve top search result links
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-# Open a browser tab for each result
-links = soup.select('.package-snippet')
-tabsOpen = min(5, len(links)) #change the number depending on number of results you want
-for i in range(tabsOpen):
-    urltoOpen = 'https://pypi.org' + links[i].get('href')
-    print('Opening', urltoOpen)
-    webbrowser.open(urltoOpen)
+    # Retrieve top search result links
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    # Open a browser tab for each result
+    links = soup.select('.package-snippet')
+    tabsOpen = min(num_tabs, len(links)) # change the number depending on number of results you want
+    for i in range(tabsOpen):
+        urltoOpen = 'https://pypi.org' + links[i].get('href')
+        print('Opening', urltoOpen)
+        webbrowser.open(urltoOpen)
+
+if __name__ == "__main__":
+    main()
